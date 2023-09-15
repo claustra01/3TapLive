0cb822a6e1be922bb32a4400aec066018e39ec6b
claustra01 <shinya430.g@gmail.com>
2023-09-15 21:51:18 +0900

    :sparkles: diesel db connection

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,30 +4,35 @@ use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
 use objects::{SimpleQuerySchema, query::Query};
 
 mod objects;
+mod db;
 
 async fn index(schema: web::Data<SimpleQuerySchema>, req: GraphQLRequest) -> GraphQLResponse {
-    schema.execute(req.into_inner()).await.into()
+  schema.execute(req.into_inner()).await.into()
 }
 
 async fn index_graphiql() -> Result<HttpResponse> {
-    Ok(HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(GraphiQLSource::build().endpoint("/").finish()))
+  Ok(HttpResponse::Ok()
+      .content_type("text/html; charset=utf-8")
+      .body(GraphiQLSource::build().endpoint("/").finish()))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
 
-    println!("GraphiQL: http://localhost:8080");
+  // DB接続テスト: 後で消す
+  db::connection().expect("Failed to connect to database");
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(Data::new(schema.to_owned()))
-            .service(web::resource("/").guard(guard::Post()).to(index))
-            .service(web::resource("/").guard(guard::Get()).to(index_graphiql))
-    })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+  let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+
+  println!("GraphiQL: http://localhost:8080");
+
+  HttpServer::new(move || {
+      App::new()
+          .app_data(Data::new(schema.to_owned()))
+          .service(web::resource("/").guard(guard::Post()).to(index))
+          .service(web::resource("/").guard(guard::Get()).to(index_graphiql))
+  })
+  .bind(("0.0.0.0", 8080))?
+  .run()
+  .await
 }
